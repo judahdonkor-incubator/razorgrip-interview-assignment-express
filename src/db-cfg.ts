30e78adef8c22a05179dbf36cbd16a5e52f0ed31
judahdonkor <judahdonkor@gmail.com>
2021-06-06 19:41:48 +0000

    Try heroku fix

--- src/db-cfg.ts
+++ src/db-cfg.ts
@@ -4,11 +4,7 @@ import { Message } from './chat'
 
 export const cfg: ConnectionOptions = {
     type: "postgres",
-    host: process.env.POSTGRES_HOST || "localhost",
-    port: Number(process.env.POSTGRES_PORT) || 5432,
-    username: process.env.POSTGRES_USER || "razorgrip",
-    password: process.env.POSTGRES_PASSWORD || "razorgrip",
-    database: process.env.POSTGRES_DB || "razorgrip",
+    url: process.env.DATABASE_URL,
     entities: [Message, BlockedUser, Online],
     synchronize: true,
 }
